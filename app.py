986d9446d19194aaf0ae8096cf1fa8ac87c1c0f5
Vilarcode <ltcvilar@gmail.com>
2025-04-04 16:43:41 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+print("Olá Mundo")
+print ("Inicio da Jornada na programação para entrar na area de Cyber securyti")
+print ("vocação")
+caixa= 'lapis'
+print (caixa)
+nome="Rânica Leticia" 
+idade=23
+altura=1.75
+estudante=True
+print(nome,idade,altura,estudante)
+mensagem="Inicio do curso Codigo da transformação"
+print(mensagem.upper())
+print(mensagem.lower())
+print(mensagem.strip())
+nome=input('qual é o seu nome? ')
+print(f"ola,{nome}")
+from datetime import datetime
+nome= input('qual o seu nome?')
+agora= datetime.now()
+hora_atual=agora.strftime('%H:%M')
+print(f'ola, {nome}! agora são {hora_atual}')
